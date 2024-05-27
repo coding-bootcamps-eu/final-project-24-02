3f1c26ebd9fefcecae90e58fbdebfca01f6478c1
Mohammad Shirazi <m.shirazi040@gmail.com>
2024-05-27 17:22:23 +0200

    update formview

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -4,13 +4,29 @@
   </article>
 
   <main>
-    <form></form>
+    <form @submit.prevent="handleSubmit">
+      <input v-model="firstname" type="text" placeholder="Enter your firstname" />
+      <input v-model="lastname" type="text" placeholder="Enter your lastname" />
+      <input v-model="email" type="email" placeholder="Enter your email" />
+      <select v-model="selectedoptions" id="options" name="options">
+        <option value="option1">For Roockies</option>
+        <option value="option2">For Wild Gymers</option>
+        <option value="option3">For Gym Freaks</option>
+      </select>
+      <button>Submit</button>
+    </form>
 
     <div class="footer-btn">
       <button @click="back">Back</button>
-      <button>Submit</button>
     </div>
   </main>
+
+  <ul>
+    <li v-for="user of users" :key="user.id">
+      {{ user.firstname }}
+    </li>
+  </ul>
+
   <div>
     <a href="#header-title">
       <span class="material-symbols-outlined"> keyboard_double_arrow_up </span>
@@ -20,7 +36,28 @@
 
 <script>
 export default {
+  data() {
+    return {
+      users: [
+        {
+          firstname: '',
+          lastname: '',
+          email: '',
+          selectedoptions: []
+        }
+      ]
+    }
+  },
   methods: {
+    handleSubmit() {
+      const user = {
+        title: this.firstname,
+        lastname: this.lastname,
+        email: this.email,
+        selectedoptions: this.selectedoptions
+      }
+      this.users.push(user)
+    },
     back() {
       this.$router.go(-1)
     }
@@ -29,6 +66,12 @@ export default {
 </script>
 
 <style scoped>
+ul {
+  background: red;
+}
+li {
+  color: white;
+}
 article {
   display: flex;
   justify-content: center;
@@ -55,7 +98,56 @@ h2 {
 }
 
 button {
-  padding: 1rem 3rem;
+  width: 80%;
+  height: 50px;
   margin-bottom: 1rem;
+  font-size: 1.2rem;
+}
+form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+}
+
+input {
+  margin: 1rem;
+  height: 50px;
+  width: 80%;
+  border: none;
+  border-radius: 4px;
+  background: transparent;
+  border-bottom: 2px solid var(--button);
+  color: var(--text);
+  font-size: 1.2rem;
+  padding-left: 0.5rem;
+}
+
+input:focus {
+  outline: none;
+  box-shadow: 0 0 8px var(--button);
+}
+
+input::placeholder {
+  color: var(--text);
+  opacity: 0.5;
+}
+
+select {
+  border: none;
+  border-radius: 4px;
+  margin: 1rem;
+  height: 50px;
+  width: 80%;
+  background: transparent;
+  border-bottom: 2px solid var(--button);
+  color: var(--text);
+  font-size: 1.2rem;
+}
+
+select:focus {
+  outline: none;
+  box-shadow: 0 0 8px var(--button);
 }
 </style>
