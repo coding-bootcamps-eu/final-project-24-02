9c9f04b6c2ea4c12e095d6f584afc814b8bcefba
Mohammad Shirazi <m.shirazi040@gmail.com>
2024-05-28 11:27:17 +0200

    add userdata component and userview

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import OfferView from '../views/OfferView.vue'
 import FormView from '../views/FormView.vue'
+import UserView from '../views/UserView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/form',
       name: 'FormView',
       component: FormView
+    },
+    {
+      path: '/user/:id',
+      name: 'UserView',
+      component: UserView
     }
   ]
 })
---
+++ src/views/UserView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h3>Welcome to your Page</h3>
+    <p>Your id is: {{ $route.params.id }}</p>
+    <p>{{ users.firstname }}</p>
+  </div>
+
+  <ul>
+    <li v-for="user of users" :key="user.id">
+      {{ users.firstname }}
+      {{ users.lastname }}
+      {{ users.email }}
+      {{ users.selectedoptions }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import UserData from '@/components/UserData.vue'
+export default {
+  components: {
+    UserData
+  }
+}
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 50vh;
+  background: red;
+}
+</style>
